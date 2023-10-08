9b0deb82b3486a47c403a115ace900f15fd2c4ca
Himanshu Agarwal <himanshuaaagarwal2002@gmail.com>
2023-10-08 20:31:02 +0530

    [TEST]

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     <div name="about" className="flex font-sans w-full h-screen">
       <div className="md:mt-[150px] max-w-screen-lg flex flex-col mx-auto justify-start p-8 text-black text-4xl w-full h-full">
         <div className='pb-8'>
-          <p className='text-4xl font-bold border-gray-500'>Welcome to Good First Issues!!</p>
+          <p className='text-4xl font-bold border-gray-500'>Welcome to Good First Issues!</p>
         </div>
       </div>
     </div>
